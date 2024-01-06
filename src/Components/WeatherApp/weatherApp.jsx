34383d0fd1225a3f128d64ae16ca61655718726a
mitanvir05 <tanvir.cse05@gmail.com>
2024-01-07 00:36:34 +0600

    project setup

---
+++ src/Components/WeatherApp/weatherApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './weatherApp.css'
+import search_icon from '../Assets/search.png'
+import clear_icon from '../Assets/clear.png'
+import cloud_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+import humidity_icon from '../Assets/humidity.png'
+const weatherApp = () => {
+  return (
+    <div>weatherApp</div>
+  )
+}
+
+export default weatherApp
